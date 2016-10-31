f987adb6d92e4b00b48517010eb2479bd12a2394
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-31 14:45:53 +0100

    Make the shoddy truth that we think to know a little more transparent.

--- stats.sh
+++ stats.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+TOTAL=$((68*6236))
 RATE=0
 while true; do
   clear
-  N1=$(ls -1 */*/* | grep -v 'quran' | grep -v -e '^$' | wc -l)
-  echo "At $(date): $N1 of probably around 421600 pages downloaded (~$RATE/min)."
+  N1=$(find quran_* -type f | wc -l)
+  echo "At $(date): $N1 of probably around $TOTAL pages downloaded (~$RATE/min)."
   /sbin/ifconfig | grep 'iB'
   sleep 60
-  N2=$(ls -1 */*/* | grep -v 'quran' | grep -v -e '^$' | wc -l)
+  N2=$(find quran_* -type f | wc -l)
   RATE=$((N2-N1))
 done
