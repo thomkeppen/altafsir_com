39692d4d2226d312090e6a46300c11100c65f094
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-31 14:47:22 +0100

    Not needed any longer.

--- commit_incrementally.sh
+++ commit_incrementally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type d -name 'madhab_??' -exec sh -c "echo {} ; git add {} && git commit --untracked-files=no {} -m 'Last couple of days worth of downloading' && git push origin HEAD:master" \;
+find . -type d -name 'madhab_??' -exec sh -c "echo {} ; git add {} && git commit --untracked-files=no {} -m 'Last couple of days worth of downloading' && git push" \;
